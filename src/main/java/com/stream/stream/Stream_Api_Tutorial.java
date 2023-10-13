469ddfbe9272d4b6a726d9dc32c873982df9f702
Dipankar Bhaduri <dipankarbhaduri8@gmail.com>
2023-10-13 08:36:17 +0530

    unwanted import removed

--- src/main/java/com/stream/stream/Stream_Api_Tutorial.java
+++ src/main/java/com/stream/stream/Stream_Api_Tutorial.java
@@ -7,10 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/rest")
